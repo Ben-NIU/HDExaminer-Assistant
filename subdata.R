a09dd3ddf03489faf5c5d9677c1eacf163ed9656
benniu720 <apple115521@hotmail.com>
2016-10-27 20:47:38 -0500

    Oct-27-16

--- subdata.R
+++ subdata.R
@@ -9,6 +9,5 @@ for(t in which){
   sub0<-subset(plot.data, Sequence==as.character(pep.map$Sequence[t]) & Charge==pep.map$Charge[t])
   sub0$peptide<-paste(pep.map$Start[t],"-",pep.map$End[t],"   ","(+",pep.map$Charge[t],")",sep="")
   sub<-rbind(sub,sub0)}
-gp<-reorder(unique(sub$peptide), 1:length(unique(sub$peptide)))
-sub$peptide<-rep(gp, each=2*length(strsplit(time.points, ",", fixed = TRUE)[[1]]))
+sub$peptide<-factor(sub$peptide, level=unique(sub$peptide))
 sub}
